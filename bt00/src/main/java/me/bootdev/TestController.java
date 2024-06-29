a5f498d0189eb76e3c8d857004a90cb769e99567
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-29 12:43:19 +0900

    Spring Boot 02

--- bt00/src/main/java/me/bootdev/TestController.java
+++ bt00/src/main/java/me/bootdev/TestController.java
@@ -1,14 +1,27 @@
 package me.bootdev;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+    // 프레젠테이션 계층
+    @Autowired
+    TestService testService;
+//    @GetMapping("/test1")
+//    public String test(){
+//        return "Hello World";
+//    }
 
     @GetMapping("test")
-    public String test(){
-        return "Hello World";
+    public List<Member> getAllMembers(){
+        List<Member> members = testService.getAllMembers();
+
+        return members;
     }
+
 }
--- bt00/src/main/java/me/bootdev/TestService.java
+++ bt00/src/main/java/me/bootdev/TestService.java
@@ -1,7 +1,19 @@
 package me.bootdev;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TestService {
+    // 비즈니스 계층
+
+    // Bean 주입
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers(){
+        return memberRepository.findAll();
+    }
 }
