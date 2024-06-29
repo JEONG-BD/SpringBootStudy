a85165c524126a5dbd7ce1fa7b312b8a9e72974a
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-29 14:35:55 +0900

    Entity

--- bt00/src/main/java/me/bootdev/Member.java
+++ bt00/src/main/java/me/bootdev/Member.java
@@ -2,11 +2,12 @@ package me.bootdev;
 
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Entity
