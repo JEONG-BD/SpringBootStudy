b2b7cd9511d6887900709b9b581a495f1b075574
JEONG-BD <dev.byeongdu@gmail.com>
2024-07-02 18:17:23 +0900

    Aircraft Domain

---
+++ bt04/src/test/java/com/example/bt04/Bt04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bt04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
