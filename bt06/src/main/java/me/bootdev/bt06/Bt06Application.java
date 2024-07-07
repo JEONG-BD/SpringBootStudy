fe79219b06a14f4afb4adb8746b23666ae3bb3fc
JEONG-BD <dev.byeongdu@gmail.com>
2024-07-07 15:51:08 +0900

    Spring security

---
+++ bt06/src/main/java/me/bootdev/bt06/Bt06Application.java
@@ -0,0 +1,13 @@
+package me.bootdev.bt06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bt06Application.class, args);
+    }
+
+}
