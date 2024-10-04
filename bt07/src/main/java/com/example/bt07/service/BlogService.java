3dde55de3fade9cd8eb450d3cdf0ed126fa436ae
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-04 11:20:12 +0900

    BlogService save

---
+++ bt07/src/main/java/com/example/bt07/service/BlogService.java
@@ -0,0 +1,20 @@
+package com.example.bt07.service;
+
+import com.example.bt07.domain.Article;
+import com.example.bt07.dto.AddArticleRequest;
+import com.example.bt07.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BlogService {
+
+    private BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+
+
+}
