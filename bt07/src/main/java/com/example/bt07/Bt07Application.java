d321770626e6a9ed654c6195ffa59e7fe94bac94
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-04 12:48:38 +0900

    EnableJpaAuditing

--- bt07/src/main/java/com/example/bt07/Bt07Application.java
+++ bt07/src/main/java/com/example/bt07/Bt07Application.java
@@ -2,7 +2,9 @@ package com.example.bt07;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Bt07Application {
 
