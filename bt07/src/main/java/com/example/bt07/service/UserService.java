6cfa2a14dca4f401654b758ac55762514422698a
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-04 11:15:27 +0900

    Article domain

---
+++ bt07/src/main/java/com/example/bt07/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.bt07.service;
+
+import com.example.bt07.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+}
