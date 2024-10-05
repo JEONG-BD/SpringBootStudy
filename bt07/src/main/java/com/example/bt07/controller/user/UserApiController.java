1db4d5d7db5f109a9ff1005ff109641bd80ff36a
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-05 11:47:34 +0900

    log out

--- bt07/src/main/java/com/example/bt07/controller/user/UserApiController.java
+++ bt07/src/main/java/com/example/bt07/controller/user/UserApiController.java
@@ -2,9 +2,14 @@ package com.example.bt07.controller.user;
 
 import com.example.bt07.dto.user.AddUserRequest;
 import com.example.bt07.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -18,6 +23,13 @@ public class UserApiController {
     public String singUp(AddUserRequest request){
 
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+
         return "redirect:/login";
     }
 
